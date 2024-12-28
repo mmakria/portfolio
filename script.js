87ed96d8a071a6a5f8b676f803c36229a74003dc
momomak <m.makria@gmail.com>
2024-12-28 20:42:32 +0100

    premiere version avec bg colorful gsapp

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let mouseFollower = document.getElementById("mouseFollow");
 
 document.addEventListener("mousemove", function (e) {
-  // mouseX = e.screen
+    // mouseX = e.screen
 
-  let mouseX = e.pageX;
-  let mouseY = e.pageY;
-  mouseFollower.style.left = mouseX + "px";
-  mouseFollower.style.top = mouseY + "px";
+    let mouseX = e.pageX;
+    let mouseY = e.pageY;
+    mouseFollower.style.left = mouseX + "px";
+    mouseFollower.style.top = mouseY + "px";
 });
 
 const title = document.querySelector("h1");
@@ -15,36 +15,47 @@ const title = document.querySelector("h1");
 // Gsap test
 
 
+const title1 = document.querySelector("h1")
+const heroContent = document.querySelector(".hero-content")
+const navigation = document.querySelector("nav")
+const sayHi = document.getElementById("say-hi-nav")
 
 
-gsap.from(".card-container-1", {
-  x: "-50%",
-  duration: 1,
-});
+const tl = gsap.timeline()
+
+tl
 
 
-gsap.from("h1", {
-  x: "50%",
-  duration: 1,
-})
-gsap.from("#screen-laptop", {
-  scrollTrigger:{
-    trigger: "#screen-laptop",
-    start: "top center",
-    markers: true,
+    .to(title1, {autoAlpha: 1})
+    .to(heroContent, {autoAlpha: 1})
+    .to(navigation, {autoAlpha: 1})
+    .to(sayHi, {autoAlpha: 1})
 
-  }, 
-  x: "50%",
-  duration: 1,
-  
-  
-})
-gsap.from(".container-2-text", {
-  scrollTrigger:{
-    trigger: "#screen-laptop",
-    start: "top 80%",
-  }, 
-  x: "-50%",
-  duration: 1,
-  
-})
+
+const bg = document.querySelector("body")
+
+gsap.fromTo(bg, {
+        backgroundSize: "100%",
+
+
+
+    },
+
+    {
+        backgroundSize: "130%",
+        backgroundPosition: "50% 100%",
+        repeat: -1,
+        duration: 30,
+        ease: "none",
+        filter: "blur(20) saturate(0.5) brightness(2)",
+        yoyo: true,
+
+
+
+
+
+
+    })
+
+
+
